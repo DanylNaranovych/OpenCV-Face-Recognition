cbc68db03a2ceeec3adb982b08d39cb2b7bdbfa9
DanylNaranovych <Danylo.Naranovych@cit.khpi.edu.ua>
2024-10-10 20:52:08 +0300

    Добавьте файлы проекта.

---
+++ CMakeProject1.cpp
@@ -0,0 +1,65 @@
+﻿// CMakeProject1.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeProject1.h"
+
+int main() {
+    cv::cuda::setDevice(0);
+    cv::VideoCapture cap(0);
+    cv::Mat frame, prevFrame, diffFrame;
+    cv::cuda::GpuMat Gframe, GprevFrame, GdiffFrame;
+    std::string filename;
+    unsigned int counter = 0;
+
+    // Check for a camera
+    if (!cap.isOpened()) {
+        std::cout << "No camera exist\n";
+        return -1;
+    }
+
+    // Capture the first frame
+    cap >> frame;
+    Gframe.upload(frame);
+    cv::cuda::cvtColor(Gframe, GprevFrame, cv::COLOR_BGR2GRAY);
+
+    while (1) { // Basic processing cycle
+        cap >> frame;
+        Gframe.upload(frame);
+        cv::cuda::cvtColor(Gframe, Gframe, cv::COLOR_BGR2GRAY);
+
+        // Compute the absolute difference between the current frame and the previous frame
+        cv::cuda::absdiff(Gframe, GprevFrame, GdiffFrame);
+
+        // Download the difference frame to the CPU
+        GdiffFrame.download(diffFrame);
+
+        // Threshold the difference to get the motion areas
+        cv::threshold(diffFrame, diffFrame, 50, 255, cv::THRESH_BINARY);
+
+        // Check if there is any motion
+        if (cv::countNonZero(diffFrame) > 0) {
+            // Display the result if motion is detected
+            cv::imshow("Motion Detection", diffFrame);
+
+            // Save the original frame to disk
+            filename = std::string("D:/WorkProjects/CMakeProject1/collectedPictures/motion_detected_frame_") + std::to_string(counter++) + std::string(".jpg");
+            // cv::imwrite(filename, frame);
+            std::cout << "Motion detected! Frame saved to " << filename << "\n";
+        }
+        else {
+            // Perform other actions if no motion is detected
+            std::cout << "No motion detected\n";
+        }
+
+        // Update the previous frame
+        Gframe.copyTo(GprevFrame);
+
+        if (cv::waitKey(1) == 27) { // Press esc
+            std::cout << "End camera loop\n";
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
---
+++ CMakeProject1.h
@@ -0,0 +1,12 @@
+﻿// CMakeProject1.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/cudaimgproc.hpp>
+#include <opencv2/cudaarithm.hpp>
+#include <dlib/image_processing/frontal_face_detector.h>
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
