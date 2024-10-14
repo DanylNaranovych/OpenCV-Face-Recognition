431490270785c23e68aa6d6c61276f796dfbbc2e
DanylNaranovych <Danylo.Naranovych@cit.khpi.edu.ua>
2024-10-14 23:29:09 +0300

    It`s working, but slow, needs a little optimization. and add some work with the database.

--- OpenCV-Face-Recognition.cpp
+++ OpenCV-Face-Recognition.cpp
@@ -4,36 +4,36 @@
 // of saving the last frame before starting the processing of saved photos
 class Timer {
 public:
-	Timer() : startTime(std::chrono::steady_clock::now()) {}
+	Timer() : startTime(chrono::steady_clock::now()) {}
 
 	// Timer reset
 	void reset() {
-		startTime = std::chrono::steady_clock::now();
+		startTime = chrono::steady_clock::now();
 	}
 
 	// Returns the time in seconds since the last reset or startup
 	double elapsed() const {
-		auto currentTime = std::chrono::steady_clock::now();
-		std::chrono::duration<double> elapsedTime = currentTime - startTime;
+		auto currentTime = chrono::steady_clock::now();
+		chrono::duration<double> elapsedTime = currentTime - startTime;
 		return elapsedTime.count();
 	}
 
 private:
-	std::chrono::steady_clock::time_point startTime;
+	chrono::steady_clock::time_point startTime;
 };
 
 // Function for getting counter
-int getLastFrameNumber(const std::string& directoryPath, const std::string& patternPart) {
-	std::regex filePattern(std::format(R"({}_(\d+))", patternPart));
-	std::string filename;
-	std::smatch match;
+int getLastFrameNumber(const string& directoryPath, const string& patternPart) {
+	regex filePattern(format(R"({}_(\d+))", patternPart));
+	string filename;
+	smatch match;
 	int lastFrameNumber = -1;
 
 	for (const auto& entry : fs::directory_iterator(directoryPath)) {
 		if (entry.is_regular_file()) {
 			filename = entry.path().filename().string();
-			if (std::regex_search(filename, match, filePattern)) {
-				int frameNumber = std::stoi(match[1].str());
+			if (regex_search(filename, match, filePattern)) {
+				int frameNumber = stoi(match[1].str());
 				if (frameNumber > lastFrameNumber) {
 					lastFrameNumber = frameNumber;
 				}
@@ -44,55 +44,72 @@ int getLastFrameNumber(const std::string& directoryPath, const std::string& patt
 	return lastFrameNumber;
 }
 
-void processCollectedPictures(dlib::frontal_face_detector& detector, dlib::shape_predictor& pose_model, int imgIndex) {
+void processCollectedPictures(frontal_face_detector& detector, shape_predictor& pose_model, anet_type& face_recognizer, int imgIndex) {
 	bool knownPerson = false;
-	std::string personName, identifiedPersonName;
-	std::string filePath = COLLECTED_DIR + std::string("motion_detected_frame_" + std::to_string(imgIndex) + ".jpg");
+	short int index;
+	string personName, identifiedPersonName;
+	string filePath = COLLECTED_DIR + string("motion_detected_frame_" + to_string(imgIndex) + ".jpg");
 	cv::Mat img = cv::imread(filePath);
 
 	if (img.empty()) return;
 
-	dlib::cv_image<dlib::bgr_pixel> cimg(img);
-	std::vector<dlib::rectangle> faces = detector(cimg);
+	cv_image<bgr_pixel> cimg(img);
+	std::vector<rectangle> faces = detector(cimg);
 
 	// Checking the presence of faces on the frame
 	if (!faces.empty()) {
 		personName = "unknownPerson";
+
+		// Извлекаем дескриптор для лица на текущем изображении
+		matrix<rgb_pixel> face_chip;
+		full_object_detection shape = pose_model(cimg, faces[0]);
+		extract_image_chip(cimg, get_face_chip_details(shape, 150, 0.25), face_chip);
+		matrix<float, 0, 1> face_descriptor = face_recognizer(face_chip);
 
 		for (const auto& personEntry : fs::directory_iterator(IDENTIFIED_DIR)) {
 			identifiedPersonName = personEntry.path().filename().string();
 
 			for (const auto& imgEntry : fs::directory_iterator(IDENTIFIED_DIR + identifiedPersonName)) {
 				cv::Mat knownImg = cv::imread(imgEntry.path().string());
-				dlib::cv_image<dlib::bgr_pixel> knownCimg(knownImg);
-				std::vector<dlib::rectangle> knownFaces = detector(knownCimg);
+				cv_image<bgr_pixel> knownCimg(knownImg);
+				std::vector<rectangle> knownFaces = detector(knownCimg);
 
-				if (!knownFaces.empty() && knownFaces.size() == faces.size()) {
-					dlib::full_object_detection knownShape = pose_model(knownCimg, knownFaces[0]);
-					dlib::full_object_detection newShape = pose_model(cimg, faces[0]);
+				if (!knownFaces.empty()) {
+					full_object_detection knownShape = pose_model(knownCimg, knownFaces[0]);
+					extract_image_chip(knownCimg, get_face_chip_details(knownShape, 150, 0.25), face_chip);
+					matrix<float, 0, 1> known_face_descriptor = face_recognizer(face_chip);
 
-					// Простое сравнение по количеству точек на лице
-					if (knownShape.num_parts() == newShape.num_parts()) {
+					// Сравниваем дескрипторы лиц с помощью эвклидовой дистанции
+					double distance = length(face_descriptor - known_face_descriptor);
+					if (distance < 0.6) { // Пороговое значение для определения похожих лиц
 						knownPerson = true;
 						personName = identifiedPersonName;
 						break;
 					}
 				}
+				if (knownPerson) break;
 			}
 			if (knownPerson) break;
 		}
 
 		if (knownPerson) {
-			std::string destination = IDENTIFIED_DIR + personName + "/" + personName + "_" + std::to_string(getLastFrameNumber(IDENTIFIED_DIR + personName + "/", personName) + 1) + ".jpg";
-			fs::rename(filePath, destination);
+			index = getLastFrameNumber(IDENTIFIED_DIR + personName + "/", personName);
+			if (index < 49) {
+				string destination = IDENTIFIED_DIR + personName + "/" + personName + "_" + to_string(++index) + ".jpg";
+				fs::rename(filePath, destination);
+			}
+			else {
+				fs::remove(filePath);
+			}
 		}
 		else {
-			std::string destination = UNIDENTIFIED_DIR + personName + "_" + std::to_string(getLastFrameNumber(UNIDENTIFIED_DIR, personName) + 1) + ".jpg";
+			index = getLastFrameNumber(UNIDENTIFIED_DIR, personName);
+			string destination = UNIDENTIFIED_DIR + personName + "_" + to_string(++index) + ".jpg";
 			fs::rename(filePath, destination);
 		}
 	}
 	else {
-		std::cout << "No faces detected in the file: " << filePath << std::endl;
+		cout << "No faces detected in the file: " << filePath << endl;
 		fs::remove(filePath);
 	}
 }
@@ -102,19 +119,21 @@ int main() {
 	cv::VideoCapture cap(0);
 	cv::Mat frame, prevFrame, diffFrame;
 	cv::cuda::GpuMat Gframe, GprevFrame, GdiffFrame;
-	std::string filename;
-	std::vector<dlib::rectangle> faces;
+	string filename;
+	std::vector<rectangle> faces;
 	Timer timer;
 	short int counter = getLastFrameNumber(COLLECTED_DIR, "motion_detected_frame");
 
-	dlib::frontal_face_detector detector = dlib::get_frontal_face_detector();
-	dlib::shape_predictor pose_model;
-	dlib::full_object_detection shape;
-	dlib::deserialize(PROJECT_DIR + std::string("shape_predictor_68_face_landmarks.dat")) >> pose_model;
+	frontal_face_detector detector = get_frontal_face_detector();
+	shape_predictor pose_model;
+	anet_type face_recognizer;
+	deserialize(PROJECT_DIR + string("shape_predictor_68_face_landmarks.dat")) >> pose_model;
+	deserialize(PROJECT_DIR + string("dlib_face_recognition_resnet_model_v1.dat")) >> face_recognizer;
+
 
 	// Check for a camera
 	if (!cap.isOpened()) {
-		std::cout << "No camera exist\n";
+		cout << "No camera exist\n";
 		return -1;
 	}
 
@@ -141,21 +160,21 @@ int main() {
 		// Display the result
 		cv::imshow("Motion Detection", diffFrame);
 
-		std::cout << "counter: " << counter << " time passed: " << timer.elapsed() << std::endl;
+		cout << "counter: " << counter << " time passed: " << timer.elapsed() << endl;
 
 		// Check if there is any motion
 		if (cv::countNonZero(diffFrame) > 0) {
 			// Save the original frame to disk
-			filename = COLLECTED_DIR + std::string("motion_detected_frame_") + std::to_string(++counter) + ".jpg";
+			filename = COLLECTED_DIR + string("motion_detected_frame_") + to_string(++counter) + ".jpg";
 			cv::imwrite(filename, frame);
-			std::cout << "Motion detected! Frame saved to " << filename << "\n";
+			cout << "Motion detected! Frame saved to " << filename << "\n";
 			timer.reset();
 		}
 		// Check for frames in folder and whether 5 seconds have elapsed since the last motion
 		else if (counter >= 0 && timer.elapsed() >= 5) {
 			// Perform face detection if no motion is detected
-			std::cout << "No motion detected. Start processing collected frames\n";
-			processCollectedPictures(detector, pose_model, counter);
+			cout << "No motion detected. Start processing collected frames\n";
+			processCollectedPictures(detector, pose_model, face_recognizer, counter);
 
 			counter = getLastFrameNumber(COLLECTED_DIR, "motion_detected_frame");
 		}
@@ -165,7 +184,7 @@ int main() {
 
 		// Press esc to close the program
 		if (cv::waitKey(1) == 27) {
-			std::cout << "End camera loop\n";
+			cout << "End camera loop\n";
 			return 1;
 		}
 	}
